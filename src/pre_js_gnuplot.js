4e83fe1718df6b2f54b2739dad478f232c4e05e4
Georg@Atlas <gthb@filios.de>
2023-07-09 19:03:05 +0200

    Trying to Move Stdin

--- src/pre_js_gnuplot.js
+++ src/pre_js_gnuplot.js
@@ -1,5 +1,17 @@
+const stdin = function writeToStdIn(buf) {
+    console.log(buf);
+    return function() {
+        if (!buf.length) {
+            return null;
+        }
+        const c = buf[0];
+        buf = buf.slice(1);
+        return c;
+    };
+};
+
 var Module = {
-    'arguments': ['-e', ""],
+    'arguments': ['', ""],
     'postRun': function() { runCommand()},
     'print': function (text) {
       handleGnuplotOutput(text);
@@ -9,6 +21,40 @@ var Module = {
       const textnode = document.createTextNode(text);
       node.appendChild(textnode);
       document.getElementById("output").appendChild(node);
-    }
+    },
+    preRun: function() {
+        function stdin(input) {
+          if (i < res.length) {
+            var code = input.charCodeAt(i);
+            ++i;
+            return code;
+          } else {
+            return null;
+          }
+        }
+    
+        var stdoutBuffer = "";
+        function stdout(code) {
+          if (code === "\n".charCodeAt(0) && stdoutBuffer !== "") {
+            console.log(stdoutBuffer);
+            stdoutBuffer = "";
+          } else {
+            stdoutBuffer += String.fromCharCode(code);
+          }
+        }
+    
+        var stderrBuffer = "";
+        function stderr(code) {
+          if (code === "\n".charCodeAt(0) && stderrBuffer !== "") {
+            console.log(stderrBuffer);
+            stderrBuffer = "";
+          } else {
+            stderrBuffer += String.fromCharCode(code);
+          }
+        }
+    
+        FS.init(stdin, stdout, stderr);
+      }
+
   };
   
